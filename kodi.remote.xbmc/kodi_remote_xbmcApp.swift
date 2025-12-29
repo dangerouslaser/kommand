8de44d33ef919074370b67a2db8265a82e8d57d3
Bryan Hoban <bryan.hoban@gmail.com>
2025-12-29 18:34:33 -0500

    Initial Commit

---
+++ kodi.remote.xbmc/kodi_remote_xbmcApp.swift
@@ -0,0 +1,17 @@
+//
+//  kodi_remote_xbmcApp.swift
+//  kodi.remote.xbmc
+//
+//  Created by Bryan Hoban on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct kodi_remote_xbmcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
